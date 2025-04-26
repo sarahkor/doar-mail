1421e10fd12d8cb87789a9d036a2484939ad7839
Or <orshalem101@gmail.com>
2025-04-26 15:42:44 +0300

    Fix: Correct ConfigurableHash hash chaining behavior

--- src/core/ConfigurableHash.cpp
+++ src/core/ConfigurableHash.cpp
@@ -3,9 +3,9 @@
 #include <functional>   
 #include <string>       
 
-ConfigurableHash::ConfigurableHash(const std::string& type, int repeat)
+ConfigurableHash::ConfigurableHash(const std::string& type, size_t repeat)
     : type(type), repeat(repeat) {
-    if (repeat <= 0) {
+    if (repeat == 0) {
         throw std::invalid_argument("Repeat must be positive");
     }
 }
@@ -13,10 +13,10 @@ ConfigurableHash::ConfigurableHash(const std::string& type, int repeat)
 size_t ConfigurableHash::hash(const std::string& input) const {
     if (type == "std") {
         std::hash<std::string> hasher;
-        size_t result = hasher(input);
+        size_t result = hasher(input); // first hash on input
 
-        for (int i = 1; i < repeat; ++i) {
-            result = hasher(input + std::to_string(result));
+        for (size_t i = 1; i < repeat; ++i) {
+            result = hasher(std::to_string(result)); // <=== hash on to_string of previous result
         }
 
         return result;
--- src/core/ConfigurableHash.h
+++ src/core/ConfigurableHash.h
@@ -1,17 +1,11 @@
-#pragma once
-
 #include "IHashFunction.h"
-#include <string>
 
-// ConfigurableHash class using std::hash repeated N times
 class ConfigurableHash : public IHashFunction {
-private:
-    std::string type;
-    int repeat;
-
-public:
-    ConfigurableHash(const std::string& type, int repeat);
-
-    // Hashes a string using the specified method and repeat count
-    size_t hash(const std::string& input) const override;
-};
+    private:
+        std::string type;
+        size_t repeat;   // <<< change from int to size_t
+    public:
+        ConfigurableHash(const std::string& type, size_t repeat);   // <<< constructor
+        size_t hash(const std::string& input) const override;
+    };
+    
