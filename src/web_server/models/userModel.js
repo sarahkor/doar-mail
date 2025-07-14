03d457ea9d926dd077b253a84f013d63217b9698
meshybb <meshybb@gmail.com>
2025-07-14 19:49:05 +0300

    Add lastName field to user model and update mail services to use display name helper

--- src/web_server/models/userModel.js
+++ src/web_server/models/userModel.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
   firstName: { type: String, required: true },
+  lastName: { type: String },
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   picture: { type: String },
